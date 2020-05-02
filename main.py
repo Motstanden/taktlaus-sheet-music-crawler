76ba5c24bfde5ac79f3b5a6b415028f5cf83758e
Torstein Wilhelmsen Sandvik <MotstandenOkononomi>
2020-05-02 22:02:50 +0200

    Secrets will now be loaded from a local .env file

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import os
+from dotenv import load_dotenv
+from typing import Tuple
 
 def clear_terminal():
     os.system('clear')
 
+def setup_environment() -> Tuple[str, str]:
+    load_dotenv(verbose=True)
+    username = os.getenv('TAKTLAUS_USERNAME')
+    password = os.getenv('TAKTLAUS_PASSWORD')
+    return username, password 
+
 if __name__ == "__main__":
     clear_terminal()
-    print('Hello World')
+    username, password = setup_environment()
+    print(username, password)
+
+    
